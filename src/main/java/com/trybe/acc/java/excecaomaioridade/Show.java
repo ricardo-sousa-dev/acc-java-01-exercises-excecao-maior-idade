4216e7306ab7dd6ea995104880a1302ec376fc63
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-03-28 09:35:44 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/excecaomaioridade/Show.java
@@ -0,0 +1,15 @@
+package com.trybe.acc.java.excecaomaioridade;
+
+/**
+ * Classe do desafio.
+ */
+public class Show {
+
+  /**
+   * Método do desafio.
+   */
+  public boolean permitirEntrada(int idade) {
+    // Altere este método
+    return false;
+  }
+}
